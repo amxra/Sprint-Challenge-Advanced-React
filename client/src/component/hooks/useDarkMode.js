2079103c01f606e02dac11a1c9f08d4a56db5f9a
amxra <amiradediran@gmail.com>
2019-10-04 12:18:42 +0100

    Create useLocalStorage and useDarkMode custom hooks

--- client/src/component/hooks/useDarkMode.js
+++ client/src/component/hooks/useDarkMode.js
@@ -1,20 +1,20 @@
-// import useLocalStorage from './useLocalStorage';
-// import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+import { useEffect } from 'react';
 
-// const useDarkMode = (initialvalue) => {
-//     const [darkMode, setDarkMode] = useLocalStorage('dark',initialvalue)
+const useDarkMode = (initialvalue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('dark',initialvalue)
 
-//     useEffect(()=>{
-//         const body = document.body;
-//         if(darkMode){
-//             body.classList.add('dark-mode');
-//         } else {
-//             body.classList.remove('dark-mode');
-//         }
-//     },[darkMode])
+    useEffect(()=>{
+        const body = document.body;
+        if(darkMode){
+            body.classList.add('dark-mode');
+        } else {
+            body.classList.remove('dark-mode');
+        }
+    },[darkMode])
 
-//     return [darkMode, setDarkMode]
+    return [darkMode, setDarkMode]
 
-// }
+}
 
-// export default useDarkMode; 
+export default useDarkMode; 
