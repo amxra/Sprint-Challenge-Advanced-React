2f9c61fca8ef9a7e9492ad3ae223ffd23c2caeea
amxra <amiradediran@gmail.com>
2019-10-04 12:07:21 +0100

    Recfactor App to class component and inject Players component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Players from './component/players'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+
+  render (){
+    <div className = 'App'>
+      <h2>Women's World Cup</h2>
+      <Players/>
     </div>
-  );
+  }
 }
 
 export default App;
